2859eaa9a159249c0484708989ef273a26622ce3
dodik affrudin <januar.rahmadiarto@satoriagroup.co.id>
2024-11-21 10:39:00 +0700

    update-components-button

--- App.tsx
+++ App.tsx
@@ -8,6 +8,8 @@
 import React from 'react';
 import type {PropsWithChildren} from 'react';
 import {
+  Alert,
+  Button,
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -24,36 +26,11 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import ButtonComponent from './components/actions/button-components';
 
 type SectionProps = PropsWithChildren<{
   title: string;
 }>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -71,26 +48,7 @@ function App(): React.JSX.Element {
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
+       <ButtonComponent title="Submit" margin={10} borderRadius={10} onPress={() => Alert.alert('Button pressed')}/>       
       </ScrollView>
     </SafeAreaView>
   );
--- components/actions/button-components.tsx
+++ components/actions/button-components.tsx
@@ -1,22 +1,42 @@
-import {Button} from 'react-native';
+import {Text, TouchableNativeFeedback, View} from 'react-native';
 
 const ButtonComponent = (props: any) => {
-  const {onPress = () => {}, title = ''} = props;
-  return <Button title={title} onPress={onPress} />;
+  const {
+    onPress = () => {},
+    title = '',
+    backgroundColor = '#2196f3',
+    color = '#fff',
+    margin = 0,
+    borderRadius = 0,
+    fontSize = 16,
+  } = props;
+  return (
+    <TouchableNativeFeedback onPress={onPress}>
+      <View
+        style={{
+          height: 50,
+          backgroundColor: backgroundColor,
+          padding: 10,
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          margin: margin,
+          borderRadius: borderRadius,
+        }}>
+        <Text
+          style={{
+            color: color,
+            width: '100%',
+            textAlign: 'center',
+            fontSize: fontSize,
+          }}>
+          {title}
+        </Text>
+      </View>
+    </TouchableNativeFeedback>
+  );
 };
 
-// const styles = StyleSheet.create({
-//   button: {
-//     width: 100,
-//     height: 100,
-//     backgroundColor: 'red',
-//     color: 'white',
-//     fontSize: 20,
-//     fontWeight: 'bold',
-//     display: 'flex',
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//   },
-// });
-
+// Example Used
+/* <ButtonComponent title="Submit" margin={10} borderRadius={10} onPress={() => Alert.alert('Button pressed')}/>*/
 export default ButtonComponent;
