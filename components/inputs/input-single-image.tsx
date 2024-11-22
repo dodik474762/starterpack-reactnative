b333e18cb3e10048a526f6d2aad63e17894befc3
unknown <solutionsdapps@gmail.com>
2024-11-22 13:44:48 +0700

    update

--- components/inputs/input-single-image.tsx
+++ components/inputs/input-single-image.tsx
@@ -24,7 +24,6 @@ const InputSingleImage = (props: any) => {
         transparent={true}
         visible={modalVisible}
         onRequestClose={() => {
-          Alert.alert('Modal has been closed.');
           setModalVisible(!modalVisible);
         }}>
         <View style={styles.centeredView}>
